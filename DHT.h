4f922c20d8e2c3dda57d2c2f10b4dea69ad7b76d
Ansifa kouser M <ansifakouser4@gmail.com>
2024-12-15 01:58:29 +0530

    DHT.h

--- DHT.h
+++ DHT.h
@@ -1,19 +1,3 @@
-/*!
- *  @file DHT.h
- *
- *  This is a library for DHT series of low cost temperature/humidity sensors.
- *
- *  You must have Adafruit Unified Sensor Library library installed to use this
- * class.
- *
- *  Adafruit invests time and resources providing this open source code,
- *  please support Adafruit andopen-source hardware by purchasing products
- *  from Adafruit!
- *
- *  Written by Adafruit Industries.
- *
- *  MIT license, all text above must be included in any redistribution
- */
 
 #ifndef DHT_H
 #define DHT_H

b19eacb7fcce5236bc29fdaa238b75e8b23ab3fd
Ansifa kouser M <ansifakouser4@gmail.com>
2024-12-15 02:00:15 +0530

    DHTtester.ino

--- examples/DHTtester/DHTtester.ino
+++ examples/DHTtester/DHTtester.ino
@@ -1,5 +1,3 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
 
 // REQUIRES the following Arduino libraries:
 // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
